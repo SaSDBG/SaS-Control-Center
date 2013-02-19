5f11adc02e5d0508f9031f1a9e15a6e2a6f25888
Maximilian Narr <maxe94@live.de>
2013-02-19 19:53:04 +0100

    added option to print company details

--- app/controllers/company.print.php
+++ app/controllers/company.print.php
@@ -8,8 +8,19 @@ use sasCC\App;
 $app->match('/companies/list/print', function (Request $r, App $app){  
     $companies = $app['em']->getRepository('sasCC\Company\Company')
                            ->findAll();
-    return $app['twig']->render('company/company.print.twig', array("title" => "Betriebsliste", "companies" => $companies));
+    return $app['twig']->render('company/company.list.print.twig', array("title" => "Betriebsliste", "companies" => $companies));
 })
 ->bind('company_list_print')
-->secure('ROLE_WIRTSCHAFT_PRIV');
+->secure('ROLE_WIRTSCHAFT_ADMIN');
+
+// Print company details
+$app->match('/companies/details/{id}/print', function (Request $r, App $app, $id){
+    $company = $app['em']->find("sasCC\Company\Company", (int)$id);
+    if($company === null) return 'Company not Found';
+    
+    return $app['twig']->render('company/company.print.twig', array("title" => "Betriebsdetails", "company" => $company));
+    
+})
+->bind('company_detail_print')
+->secure('ROLE_WIRTSCHAFT_ADMIN');
 ?>
