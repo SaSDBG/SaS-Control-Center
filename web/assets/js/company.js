c02b337f01f72012cb34a27388baba26d66aac65
Maximilian Narr <maxe94@live.de>
2013-02-23 20:51:15 +0100

    datatables now saves configuration

--- web/assets/js/company.js
+++ web/assets/js/company.js
@@ -7,8 +7,9 @@ $(document).ready(function(){
     });
            
     $("#company-list").dataTable ({
-        sDom: "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
-        "sPaginationType": "bootstrap"
+        "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+        "sPaginationType": "bootstrap",
+        "bStateSave": true
     }); 
             
     $.extend( $.fn.dataTableExt.oStdClasses, {
